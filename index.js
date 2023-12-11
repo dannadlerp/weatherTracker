f0c55ec20aa95ab8783b9563a5960d0a3deb3b49
Dan Nadler <142226474+dannadlerp@users.noreply.github.com>
2023-12-11 14:11:17 -0500

    Add files via upload
    switched from lat/lon api call to query by city api. changed variable names/locators to match new api data.

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
 let geoLat = 43.5789;
 let geoLon = 79.6583;
+let city = "Mississauga";
 const apiKey = '80b66692cc83d65b511d9584d9a227b7';
-const apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${geoLat}&lon=${geoLon}&appid=${apiKey}`;
+//const apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${geoLat}&lon=${geoLon}&appid=${apiKey}`;
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 let weatherInfo = {name: "weatherApp"};
 let hour = 21;
+ let cityVar = document.getElementById=("weather-info").textContent;
 
 //retrieve api and convert with json
- fetch(apiURL)
- .then(function (response) {
-    return response.json();
- })
-
- .then(function (data) {
-   weatherInfo.cityName = data.city.name;
-   weatherInfo.coord = data.city.coord;
-   weatherInfo.country = data.city.country;
-   weatherInfo.weatherArray = data.list;
-   weatherInfo.dateTime = data.list[hour].dt_txt;
-   weatherInfo.pop = data.list[hour].pop;
-   weatherInfo.weather = data.list[hour].weather;
-   weatherInfo.desc = data.list[hour].weather[0].main;
-   weatherInfo.detail = data.list[hour].weather[0].description;
-   weatherInfo.windDegrees = data.list[hour].wind.deg;
-   weatherInfo.windGust = data.list[hour].wind.dgust;
-   weatherInfo.windSpd = data.list[hour].wind.speed;
-   console.log(data);
-   function displayWeatherInfo() {
-      console.log(weatherInfo.cityName);
-      console.log(weatherInfo.dateTime);
-      console.log(`${100*weatherInfo.pop}% POP`);
-      console.log(weatherInfo.detail);
-      console.log(`Windspeed: ${weatherInfo.windSpd}km/h`);
-   }
+fetch(apiURL)
+.then(function (response) {
+   return response.json();
 })
-console.log(weatherInfo.pop);
 
-/* if (length(data) === 0) {
-} else {}
-console.log('nothing returned'); //checks a value has been returned
-}
-)
-console.log(weatherInfo); */
+.then(function (data) {
+   //function searchCityInfo(params) {
 
- document.getElementById("weather-info").textContent = [weatherInfo.cityName, weatherInfo.detail, `Weather is: ${weatherInfo.desc}`];
-document.getElementById("search-button").onclick
+   console.log(data);
+      weatherInfo.cityName = data.name;
+      weatherInfo.temp = Math.ceil(data.main.temp-273.15);
+      weatherInfo.wind = data.wind.speed;
+      weatherInfo.humid = data.main.humidity;
+      geoLat = data.coord.lat;
+      geoLon = data.coord.lon;
+   weatherInfo.country = data.country;
+   //weatherInfo.weatherArray = data.list;
+   //weatherInfo.dateTime = data.list[hour].dt_txt;
+  // weatherInfo.pop = data.list[hour].pop;
+     weatherInfo.desc = data.weather[0].main;
+//   weatherInfo.detail = data.list[hour].weather[0].description;
+//   weatherInfo.windDegrees = data.list[hour].wind.deg;
+//   weatherInfo.windGust = data.list[hour].wind.dgust;
+//   weatherInfo.windSpd = data.list[hour].wind.speed;
+//   function displayWeatherInfo() {  
+      console.log(weatherInfo.cityName);
+      console.log(`${weatherInfo.temp}C`);
+      console.log(weatherInfo.desc);
+      console.log(`Humidity: ${weatherInfo.wind}%`);
+      console.log(`Wind Gusts: ${weatherInfo.wind}km/H`);
+//      console.log(`Windspeed: ${weatherInfo.windSpd}km/h`);
+//      console.log(weatherInfo.pop);
+
+      document.getElementById("weather-info").textContent = [weatherInfo.temp, weatherInfo.detail, `Weather is: ${weatherInfo.desc}`];
+   //}
+const srchBtn = document.getElementById("search-button");
+
 console.log(weatherInfo);
+//}
+})
