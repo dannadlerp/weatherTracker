5dd89e7fc1effc7133e9a39d518c8dbe78be5113
Dan Nadler <142226474+dannadlerp@users.noreply.github.com>
2023-12-13 14:41:48 -0500

    Add files via upload
    added css file/folder
    added dynamic cards when button pushed (broken)

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const apiKey = '80b66692cc83d65b511d9584d9a227b7';
 //const apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${geoLat}&lon=${geoLon}&appid=${apiKey}`;
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 let weatherInfo = {name: "weatherApp"};
-let hour = 21;
- let cityVar = document.getElementById=("weather-info").textContent;
+//let hour = 21;
+
+const sunnyimg = "https://icons.iconarchive.com/icons/icons-land/weather/256/Sunny-icon.png";
+const cloudyimg = "https://static.vecteezy.com/system/resources/previews/000/441/101/original/cloudy-vector-icon.jpg";
+const rainyimg = "https://png.pngtree.com/png-clipart/20200225/original/pngtree-rain-icon-creative-design-template-png-image_5259110.jpg";
+const snowyimg = "https://previews.123rf.com/images/oldwhitewolf/oldwhitewolf1606/oldwhitewolf160600007/58313312-fully-snowy-icon-template-glossy-blue-cloud-object-glossy-blue-snowflakes-objects-snowy-icon.jpg";
+
+
+
+let weatherCard1 = "";
 
 //retrieve api and convert with json
 fetch(apiURL)
@@ -22,9 +30,9 @@ fetch(apiURL)
       weatherInfo.temp = Math.ceil(data.main.temp-273.15);
       weatherInfo.wind = data.wind.speed;
       weatherInfo.humid = data.main.humidity;
+      weatherInfo.country = data.country;
       geoLat = data.coord.lat;
       geoLon = data.coord.lon;
-   weatherInfo.country = data.country;
    //weatherInfo.weatherArray = data.list;
    //weatherInfo.dateTime = data.list[hour].dt_txt;
   // weatherInfo.pop = data.list[hour].pop;
@@ -37,15 +45,36 @@ fetch(apiURL)
       console.log(weatherInfo.cityName);
       console.log(`${weatherInfo.temp}C`);
       console.log(weatherInfo.desc);
-      console.log(`Humidity: ${weatherInfo.wind}%`);
+      console.log(`Humidity: ${weatherInfo.humid}%`);
       console.log(`Wind Gusts: ${weatherInfo.wind}km/H`);
 //      console.log(`Windspeed: ${weatherInfo.windSpd}km/h`);
 //      console.log(weatherInfo.pop);
-
-      document.getElementById("weather-info").textContent = [weatherInfo.temp, weatherInfo.detail, `Weather is: ${weatherInfo.desc}`];
-   //}
-const srchBtn = document.getElementById("search-button");
-
-console.log(weatherInfo);
 //}
+//}
+let currentWeather = sunnyimg;
+if(weatherInfo.desc === "Clouds") {
+currentWeather = cloudyimg;
+} else if(weatherInfo.desc === "Rain") {
+   currentWeather = rainyimg;
+} else if(weatherInfo.desc === "Snow") {
+   currentWeather = snowyimg;
+const loadWeatherCardData = (date, icon, temp, wind, humidity) =>  console.log(date, icon, temp, wind, humidity);
+function createCard(card) {
+card = document.createElement('div');
+card.innerHTML = temp;
+card.setAttribute('class', 'card');
+//document.createElement('/div');
+}
+let srchBtn = document.getElementById('search-button');
+      srchBtn.onclick = function clickedButton() {
+         console.log('hello');
+         loadWeatherCardData(weatherInfo.name, currentWeather, weatherInfo.temp, weatherInfo.wind, weatherInfo.humid);
+         createCard(Card1);
+      }
+      
+   console.log(weatherInfo);
+}
 })
+
+//const srchBtn = document.getElementById("search-button");
+//document.addEventListener("click", ).textContent = weatherDescription;
